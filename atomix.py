e9f34eea20006ef843073728518c3c0e075b7b55
root <root@atomix.rcentro.net>
2016-06-13 17:20:52 -0300

    add websocket

--- atomix.py
+++ atomix.py
@@ -1,6 +1,20 @@
+from sys import stdout
+from twisted.python import log
+from twisted.web.server import Site
+from twisted.web.static import File
 from twisted.internet import reactor
+from autobahn.twisted.websocket import WebSocketServerFactory, WebSocketServerProtocol
+from autobahn.twisted.resource import WebSocketResource
 from starpy.manager import AMIFactory, AMIProtocol
-import logging
+import json
+
+class WebServerProtocol(WebSocketServerProtocol):
+    servername = "atomix"
+    username = "monitor"
+    secret = "asteriskmonit"
+
+    def onConnect(self, request):
+        Atomix(self, self.servername, self.username, self.secret)
 
 class AtomixAMIFactory(AMIFactory):
 
@@ -12,11 +26,11 @@ class AtomixAMIFactory(AMIFactory):
     def connect(self):
 
         def onloginsuccess(ami):
-            log.info("Server %s :: AMI connected..." % (self.servername))
+            log.msg("Server %s :: AMI connected..." % (self.servername))
             return ami
 
         def onloginfailure(ami):
-            log.info("connection to %s failed " % (self.servername))
+            log.msg("connection to %s failed " % (self.servername))
             return ami
 
         df = self.login(ip=self.servername)
@@ -24,14 +38,15 @@ class AtomixAMIFactory(AMIFactory):
         return df
 
     def clientConnectionLost(self, connector, reason):
-        log.info("Server %s :: Lost connection to AMI: %s" % (self.servername, reason.value))
+        log.msg("Server %s :: Lost connection to AMI: %s" % (self.servername, reason.value))
 
     def clientConnectionFailed(self, connector, reason):
-        log.info("Server %s :: Failed to connected to AMI: %s" % (self.servername, reason.value))
+        log.msg("Server %s :: Failed to connected to AMI: %s" % (self.servername, reason.value))
 
 class Atomix:
 
-    def __init__(self, servername, username, secret):
+    def __init__(self, sock, servername, username, secret):
+        self.sock = sock
         self.servername = servername
         self.username = username
         self.secret = secret
@@ -40,15 +55,17 @@ class Atomix:
     def start(self):
         ami = AtomixAMIFactory(self.servername, self.username, self.secret)
         df = ami.connect()
-        df.addCallback(self.getContacts)
-        df.addCallback(self.getEvent)
+        df.addCallback(self.getContacts, self.sock)
+     #   df.addCallback(self.getEvent)
         return df
 
-    def getContacts(self, ami):
+    def getContacts(self, ami, sock):
 
         def list(result):
-            for contact in result[2:]:
-                print contact.split()[1][:3]
+            c = [contact.split()[1][:3] for contact in result[2:]]
+            d = {"Event":"Contacts", "Data":c}
+            payload = json.dumps(d, ensure_ascii=False).encode('utf8')
+            sock.sendMessage(payload, isBinary=False)
             return result
 
         df = ami.command('pjsip show contacts')
@@ -58,32 +75,34 @@ class Atomix:
     def getEvent(self, ami):
 
         def onEvent(ami, event):
-            if event.get('channelstatedesc') == 'Ring':
-                caller = event.get('calleridnum')
-                dest = event.get('exten')
-                if event.get('event') == 'Newchannel':
-                    print "%s is calling extention %s" % (caller, dest)
-                elif event.get('event') == 'Hangup':
-                    print "%s Hangup" % (caller)
+            caller = event.get('calleridnum')
+            dest = event.get('exten')
+            channel = event.get('channel')
+            if event.get('event') == 'Newchannel':
+                if caller == "<unknown>":
+                    log.msg("New external call in line %s" % channel)
+                else:
+                    log.msg("%s is calling extention %s" % (caller, dest))
+            elif event.get('event') == 'Hangup':
+                if event.get('channelstatedesc') == 'Ring':
+                    log.msg("%s Hangup" % (caller))
+            elif event.get('event') == 'DialBegin':
+                log.msg("%s is calling extention %s" % (channel, dest))
 
-        df = ami.registerEvent(None,onEvent)
+        df = ami.registerEvent(None, onEvent)
         return ami
 
-def RunAtomix(AA):
-    servername = "atomix"
-    username = "monitor"
-    secret = "asteriskmonit"
-    global log
+def RunAtomix():
+    log.startLogging(stdout)
 
-    log = logging.getLogger()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
-    log.setLevel(logging.INFO)
+    factory = WebSocketServerFactory(u"ws://127.0.0.1:8080")
+    factory.protocol = WebServerProtocol
 
-    Atomix(servername, username, secret)
+    resource = WebSocketResource(factory)
+    site = Site(resource)
+
+    reactor.listenTCP(8080, site)
     reactor.run()
 
 if __name__ == '__main__':
-    RunAtomix(Atomix)
+    RunAtomix()
